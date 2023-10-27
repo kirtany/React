10f2ba157d50055fa088aa7618fad13be93b99fb
kirtany <bhagwandinkirtanya@gmail.com>
2023-10-27 11:45:42 +0200

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from "./Weather"
-import axios from 'axios';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <p>
           Weather Search
         </p>
-        
-        
-        
-  
         <Weather/>
       </header>
     </div>
